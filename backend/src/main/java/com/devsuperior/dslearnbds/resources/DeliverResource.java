617f3c241690ee644a97275aa525facbdff4c5fe
westorres9 <wester.torres9@gmail.com>
2022-04-22 10:23:09 -0300

    Implementado metodos de pre-autorização por perfis de usuário

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dslearnbds.resources;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
+import com.devsuperior.dslearnbds.services.DeliverService;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverResource {
+	
+	@Autowired
+	private DeliverService service;
+	
+	@PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable Long id,@Valid @RequestBody DeliverRevisionDTO dto) {
+		service.saveRevision(id, dto);
+		return ResponseEntity.noContent().build();
+	}
+	
+
+}
